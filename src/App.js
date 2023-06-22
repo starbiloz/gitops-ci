8a80305c2c9a80f03163d9a339a2636ccda52de7
supachai yasotharo <supachai.y@cdg.co.th>
2023-06-22 15:37:01 +0700

    update new version

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className='App-logo' alt='logo' />
         <div style={{ fontSize: 30 }}>
           Version{' '}
-          <span style={{ color: 'red' }}>Third and FINAL Commit....!!!</span>
+          <span style={{ color: 'blue' }}>new version of react app.</span>
         </div>
       </header>
     </div>
